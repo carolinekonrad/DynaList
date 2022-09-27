04cfaf9965e391f744c2c99113b77c6ab653867b
Werefox <jack@wrightus.com>
2022-09-27 16:03:41 -0400

    Set up basic express app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,13 @@
-function Hello(str: string): void {
-  console.log(str);
-}
+import express from 'express'
+import dotenv from 'dotenv'
 
-Hello("Hello World");
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.status(200).send("Hello world")
+})
+
+app.listen(port, () => console.log(`Running server on port ${port}`))
