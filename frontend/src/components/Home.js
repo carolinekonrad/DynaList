3fe0933d2ddedfc052282592c0cde2c4720910e6
Werefox <jack@wrightus.com>
2022-09-28 18:39:05 -0400

    Added groups field to the users  model

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home() {
     return (
-        <div>
+        <div className="bg-gray-700">
             <div id="home-content">
                 <h1>DynaList</h1>
                     <p> A Shareable List for all your shopping needs.
