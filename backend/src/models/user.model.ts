08dfa3828d45c9292e4b023e5c1a784ec97ace77
Werefox <jack@wrightus.com>
2022-09-28 16:11:15 -0400

    Added fields to the user model and a pre-save function that hashes the password

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,7 +1,27 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt'
 
 const userSchema = new mongoose.Schema({
-	name: String
+	name: {
+		type: String,
+		require: true,
+	},
+	email: {
+		type: String,
+		require: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		require: true
+	}
+})
+
+userSchema.pre('save', async function (next) {
+	const hash = await bcrypt.hash(this.password!, 10)
+	this.password = hash
+
+	return next()
 })
 
 const UserModel = mongoose.model('User', userSchema)
